a0b130b11909a1e676de9872c9ddad53586c2b8c
apple <apple@apples-MacBook-Air.local>
2020-09-09 01:09:16 +0530

    added mockGenerator

---
+++ src/app/mock-generator.spec.ts
@@ -0,0 +1,7 @@
+import { MockGenerator } from './mock-generator';
+
+describe('MockGenerator', () => {
+  it('should create an instance', () => {
+    expect(new MockGenerator()).toBeTruthy();
+  });
+});
